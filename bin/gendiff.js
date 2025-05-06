9eeda5ad25538c89f8ff500a134b8a0eef1e6f9c
DenisBrez <brezgin.denis@list.ru>
2025-05-06 19:51:39 +0700

    all

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander';
-import genDiff from './gendiff.js';
+import getDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
-  .version('0.0.1')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format', 'stylish')
   .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format));
+    const diff = getDiff(filepath1, filepath2, options.format);
+    console.log(diff);
   });
 
-if (process.argv.slice(2).length === 0) {
-  program.help(); // покажет справку, но не вызовет process.exit(1)
-} else {
-  program.parse();
-}
+program.parse();
